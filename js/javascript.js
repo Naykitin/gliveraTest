59baa80c0b9b553c2f49eb6a98b570014a5e6712
Vladyslav <65622620+Naykitin@users.noreply.github.com>
2022-03-21 16:53:10 +0200

    new commit

--- js/javascript.js
+++ js/javascript.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
       $('.item__btn').removeClass('active');
       $(this).addClass('active');
       $('.item__btn').text("select plan");
-      $(this).text("selected plan");
+      $(this).text("plan selected");
 
       if ($(this).hasClass('active')) {
          $('.item__btn').closest('.item').removeClass('active');
          $(this).closest('.item').addClass('active');
       }
-    });
+   });
 })
